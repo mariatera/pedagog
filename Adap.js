a97e8f3618fd24b308423c46db671f7b0f6c486d
mariatera <130396105+mariatera@users.noreply.github.com>
2023-04-13 02:22:46 +0500

    Add files via upload

--- Adap.js
+++ Adap.js
@@ -1,17 +1,23 @@
-autoSlider();
-var left = 0;
+var polosa = document.getElementById("polosa");
+var currentImage = 0;
 var timer;
+var a = 868.6666;
 
-function autoSlider (){
-	timer = setTimeout( function () {
-		var polosa = document.getElementById ("polosa");
-	left = left - 700;
-	if (left < -3000) {
-		left=0;
-		clearTimeout(timer);
-	}
-	polosa.style.left = left+"px";
-	autoSlider ();
+function autoSlider() {
+console.log("Current Image " + currentImage);
+console.log("polosa.children.length " + polosa.children.length);
+console.log("polosa.style.left " + polosa.style.left);
+console.log("polosa.children[0].offsetWidth " + polosa.children[0].offsetWidth);
+timer = setTimeout( function () {
+  currentImage = (currentImage + 1) % polosa.children.length;
+  var b = 0;
+  if (currentImage == 0){
+  b = 36.8;}
+  var left = - (currentImage * a);
+  console.log("var left " + left);
+  polosa.style.left = left + "px";
+  autoSlider ();
 	}, 5000);
-	
 }
+
+autoSlider();
